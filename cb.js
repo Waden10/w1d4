7fa879beffc6c13ba043ac588052809da4766496
waden10 <fantm10@hotmail.com>
2017-04-06 22:35:52 +0000

    Refactor as forEach loop

--- cb.js
+++ cb.js
@@ -1,14 +1,20 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      var index = i;
-      found(index);   // execute callback
+  arr.forEach(function(name, index) {
+    if(name === 'Waldo') {
+      found(index);
     }
-  }
+  });
 }
 
+// function findWaldo(arr, found) {
+//   for (var index = 0; index < arr.length; i++) {
+//     if (arr[index] === "Waldo") {
+//       found(index);   // execute callback
+//     }
+//   }
+// }
+
 function actionWhenFound(index) {
-  
   console.log("Found Waldo at index " + index + "!");
 }
 
