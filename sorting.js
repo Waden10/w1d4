79547b54d42d7fbd56e0e1422aa7d8618f3d75e8
waden10 <fantm10@hotmail.com>
2017-04-07 04:25:06 +0000

    couldn't figure out how to get zoidberg to be below bruce

---
+++ sorting.js
@@ -0,0 +1,20 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+students.sort(function(name1, name2) {
+  if (name1.name === name2.name) {
+    return name2.age - name1.age;
+  
+  } else {
+    return 1; 
+  }
+  if (name1.name < name2.name) {
+    return -1;
+  }
+});
+console.log(students);
